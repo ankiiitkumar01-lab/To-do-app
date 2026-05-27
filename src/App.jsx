ed1d119c95c7928c2aea3a9b1e469bc8cf77608b
ankiiitkumar01-lab <ankiiitkumar01@gmail.com>
2026-05-27 22:52:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Todo from"./todo";
+
+function App(){
+  return (
+       <div>
+        <h1>TO Do APP</h1>
+        <Todo/>
+       </div>
+           
+  );
+}
+
+    
+
+
+export default App;
